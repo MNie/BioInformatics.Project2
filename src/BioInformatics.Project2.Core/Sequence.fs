dee6f0267778ab0047c863f13ef30f56587c1c4a
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2017-01-10 21:50:12 +0100

    init

---
+++ src/BioInformatics.Project2.Core/Sequence.fs
@@ -0,0 +1,41 @@
+﻿namespace BioInformatics.Project2.Core.Sequence
+open System
+type SequenceType = RNA | DNA
+
+type Sequence (seqType: SequenceType, data: string) =
+    member val Type = seqType with get
+    member val Data = data with get
+
+type SequenceParser() =
+    let ifCorrectReturnIt (alphabet: string) (data: string) (seqType: SequenceType) =
+        let isCorrect = 
+            data
+            |> Seq.filter(fun x -> x <> '\n')
+            |> Seq.map string
+            |> Seq.forall alphabet.Contains
+        if isCorrect then Option.Some (Sequence(seqType, data))
+        else Option.None
+    
+    let ifDnaReturnIt data = 
+        let dnaChars = "GCAT"
+        ifCorrectReturnIt dnaChars data SequenceType.DNA
+
+    let ifRnaReturnIt data = 
+        let dnaChars = "GCAU"
+        ifCorrectReturnIt dnaChars data SequenceType.RNA
+
+    member this.Parse(data: string, seqType: SequenceType) = 
+        match seqType with
+        | SequenceType.DNA -> ifDnaReturnIt data
+        | SequenceType.RNA -> ifRnaReturnIt data
+        | _ -> Option.None 
+
+type SequenceProvider() =
+    member this.Provide (data: string) =
+        let splitedData = data.Split '\n'
+        let sequenceData = splitedData.[1..] |> String.concat "\n"
+        let seqType = Enum.Parse(typedefof<SequenceType>, splitedData.[0]) :?> SequenceType
+        SequenceParser().Parse(sequenceData, seqType) 
+
+
+
---
+++ test/BioInformatics.Project2.Tests/SequenceSpecs.fs
@@ -0,0 +1,36 @@
+﻿module Tests.Sequence
+
+open Expecto
+open BioInformatics.Project2.Core.Sequence
+[<Tests>]
+let tests =
+    testList "SequenceParser" [
+        testCase "when trying to parse DNA" <| fun _ ->
+            let parser = SequenceParser()
+            let data = "GGG\nGATAAA"
+            let result = parser.Parse(data, SequenceType.DNA)
+
+            Expect.isSome result "should be true because passed data is correct"
+            Expect.equal result.Value.Type SequenceType.DNA "should be of type DNA"
+            Expect.equal result.Value.Data data "should parse data to seq data"
+
+        testCase "when trying to parse RNA" <| fun _ ->
+            let parser = SequenceParser()
+            let data = "GGG\nGAUAAA"
+            let result = parser.Parse(data, SequenceType.RNA)
+
+            Expect.isSome result "should be true because passed data is correct"
+            Expect.equal result.Value.Type SequenceType.RNA "should be of type RNA"
+            Expect.equal result.Value.Data data "should parse data to seq data"
+
+        testCase "when trying to parse RNA" <| fun _ ->
+            let parser = SequenceParser()
+            let data = "GGG\nGATAAA"
+            let result = parser.Parse(data, SequenceType.RNA)
+
+            Expect.isNone result "should be false because passed data is dna not rna"
+    ]
+
+[<EntryPoint>]
+let main args =
+  runTestsInAssembly defaultConfig args
